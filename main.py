41abd6c4488681fc11d47707dc531e77026a9aa3
Akira BAES <akirbaes@ulb.ac.be>
2017-05-08 14:27:26 +0200

    Graph test

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import DecisionTree
+import networkx as nx
+import numpy as np
 from parse_credits import parse_credits
+from collections import Mapping
 
+
+def draw():
+	
+	graph_data = {"root": {"level-0.A":0,
+						  "level-0.B":{"level-1.B.1":2,
+									   "level-1.B.2": {"level-2.B.2.1":3, "level-2.B.2.2":1}}}}
+	# Empty directed graph
+	G = nx.DiGraph()
+
+	# Iterate through the layers
+	q = list(graph_data.items())
+	while q:
+		v, d = q.pop()
+		for nv, nd in d.items():
+			G.add_edge(v, nv)
+			if isinstance(nd, Mapping):
+				q.append((nv, nd))
+
+	np.random.seed(8)
+	nx.draw(G, with_labels=True)
+            
 def main():
     # Insert input file
     # IMPORTANT: Change this file path to change training data
@@ -22,13 +46,22 @@ def main():
 
     attributes = data[0]
     data.pop(0)"""
-    attributes, data = parse_credits("creditcard_simplified.csv")
+    attributes, data = parse_credits("creditcard_undersampled.csv")
+    #attributes, data = parse_credits("creditcard_simplified.csv")
     target = attributes[-1]
+    
 
     # Run ID3
     tree = DecisionTree.createTree(data, attributes, target)
     print(tree)
     print("generated decision tree")
+    """
+    global graph
+    graph = pydot.Dot(graph_type='graph')
+    visit(tree)
+    graph.write_png('example1_graph.png')"""
+    draw()
+
 
 
 if __name__ == '__main__':
---
+++ parse_credits.py
@@ -0,0 +1,19 @@
+def parse_credits(filename="creditcard_simplified.csv"):
+	f = open(filename)
+	attributes = f.readline().strip().split(",")
+	
+	#database = [[] for x in classes]
+	database = []
+	
+	maxlines = float("inf") # 10000 #
+	for line in f:
+		line = line.strip().split(",")
+		line = [int(x) for x in line]
+		#for i, elem in enumerate(line):
+		#	database[i].append(elem)
+		database.append(line)
+		
+		if(len(database[0])>=maxlines):
+			break
+	f.close()
+	return attributes,database
